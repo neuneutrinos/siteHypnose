13b3e572afea9ceb78fdb20c10f48e29f164d4bd
Elisabeth <elisabeth.pluquet@gmail.com>
2021-01-26 10:17:56 +0100

    Système de login (partie envoie des données)

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let express = require('express');
+var bodyParser = require('body-parser')
 require('./test')
 let createDatabase = require('./mysql/createDatabase');
 var path = require('path');
@@ -12,6 +13,8 @@ const connection = require('./mysql/mysqlConnection');
 let app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/front/vue'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded())
 
 
 app.get('/reset',(req,res)=>
@@ -24,8 +27,17 @@ app.get('/reset',(req,res)=>
 
 app.get('/',(req,res)=>
 {
-  res.render('signin', {titre : 'BAHD'});
+  res.render('signin');
 })
+
+app.all('/signin',
+  (req, res)=> {
+    var username = req.body.username;
+    var password = req.body.password;
+    res.end()
+    console.log("post received:", username, password);
+});
+
 
 
 app.get('/test/table/select/:tableName/:id?',(req,res)=>
@@ -36,12 +48,12 @@ app.get('/test/table/select/:tableName/:id?',(req,res)=>
   connection.query(crud.readByIdQuery(),(error,result)=>
   {
     result= JSON.stringify(result)
-    
+
     let str = '<div>'+error+'</div>'
     str+='<div style="color:red">'+result+'</div>'
     res.send(str)
   })
-  
+
 
 })
 
